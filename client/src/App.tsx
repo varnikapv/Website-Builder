d26020e387598b8bb7783fb4cfc34737ecfd296e
varnikapv <pvarnika05@gmail.com>
2025-12-25 16:16:46 +0530

    home screen, myprojects and pricing

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+
+
+import { Route, Routes } from 'react-router-dom'
+import Pricing from './pages/Pricing'
+import Projects from './pages/Projects'
+import MyProjects from './pages/MyProjects'
+import Preview from './pages/Preview'
+import Community from './pages/Community'
+import View from './pages/View'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/view/:projectId" element={<View />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
